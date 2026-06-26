d421d107be9d1a3fa7d6bc1297889ee329975abd
Mohamed Elbagory <231975199+MohamedElbagoury11@users.noreply.github.com>
2026-06-26 03:16:15 +0300

    Update API base URL for axios instance

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://allsheneedsback-production-a650.up.railway.app',
+  baseURL: 'https://e-commerce-backe-production.up.railway.app',
   headers: {
     'Content-Type': 'application/json',
   },
